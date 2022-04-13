d82c783f212c013787f95499d64b17918bc51fa3
NahuelIglesias <nahueliglesias1999@gmail.com>
2022-04-12 21:28:13 -0300

    Added builder pattern and equals method for Usrr class

--- src/main/java/ar/edu/unq/desapp/grupon/backenddesappapi/Model/User.java
+++ src/main/java/ar/edu/unq/desapp/grupon/backenddesappapi/Model/User.java
@@ -1,22 +1,33 @@
+package ar.edu.unq.desapp.grupon.backenddesappapi.Model;
 
+import java.util.Objects;
 
 public class User {
+
     private Long id;
     private String name;
     private String surname;
     private String email;
     private String address;
     private String password;
-    private Integer cv;
+    private Long cvu;
     private Float reputation;
-    private Integer walletAddress;
+    private Long walletAddress;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
-        this.name = name
+        this.name = name;
     }
 
     public String getSurname() {
@@ -24,15 +35,15 @@ public class User {
     }
 
     public void setSurname(String surname) {
-        this.surname = surname
+        this.surname = surname;
     }
 
-    public String getemail() {
+    public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
-        this.email = email
+        this.email = email;
     }
     
     public String getAddress() {
@@ -40,43 +51,103 @@ public class User {
     }
 
     public void setAddress(String address) {
-        this.address = address
+        this.address = address;
     }
+
     public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
-        this.password = password
+        this.password = password;
     }
 
-
-    public String getCvu() {
+    public Long getCvu() {
         return cvu;
     }
 
-    public void setCvu(String cvu) {
-        this.cvu = cvu
+    public void setCvu(Long cvu) {
+        this.cvu = cvu;
     }
 
-
-    public String getReputation() {
+    public Float getReputation() {
         return reputation;
     }
 
-    public void setReputation(String reputatione) {
-        this.reputation = reputation
+    public void setReputation(Float reputation) {
+        this.reputation = reputation;
     }
 
-
-    public String getWalletAddress() {
+    public Long getWalletAddress() {
         return walletAddress;
     }
 
-    public void setWalletAddress(String walletAddress) {
-        this.walletAddress = walletAddress
+    public void setWalletAddress(Long walletAddress) {
+        this.walletAddress = walletAddress;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(surname, user.surname) && Objects.equals(email, user.email) && Objects.equals(address, user.address) && Objects.equals(password, user.password) && Objects.equals(cvu, user.cvu) && Objects.equals(reputation, user.reputation) && Objects.equals(walletAddress, user.walletAddress);
+    }
 
+    public static UserBuilder builder() {
+        return new UserBuilder();
+    }
+
+    public static final class UserBuilder {
+        private User user;
+
+        private UserBuilder() {
+            user = new User();
+        }
+
+        public UserBuilder name(String name) {
+            user.setName(name);
+            return this;
+        }
+
+        public UserBuilder surname(String surname) {
+            user.setSurname(surname);
+            return this;
+        }
+
+        public UserBuilder email(String email) {
+            user.setEmail(email);
+            return this;
+        }
+
+        public UserBuilder address(String address) {
+            user.setAddress(address);
+            return this;
+        }
+
+        public UserBuilder password(String password) {
+            user.setPassword(password);
+            return this;
+        }
+
+        public UserBuilder cvu(Long cvu) {
+            user.setCvu(cvu);
+            return this;
+        }
+
+        public UserBuilder reputation(Float reputation) {
+            user.setReputation(reputation);
+            return this;
+        }
+
+        public UserBuilder walletAddress(Long walletAddress) {
+            user.setWalletAddress(walletAddress);
+            return this;
+        }
+
+        public User build() {
+            return user;
+        }
+    }
 }
 
