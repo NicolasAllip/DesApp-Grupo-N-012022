ece2a31f30c1be8c3863e39639cfdae3baa56fa2
nallip <allip.nicolas@gmail.com>
2022-06-07 20:13:15 -0300

    merge with another branch

--- src/main/java/ar/edu/unq/desapp/grupon/backenddesappapi/persistence/ITransactionDao.java
+++ src/main/java/ar/edu/unq/desapp/grupon/backenddesappapi/persistence/ITransactionDao.java
@@ -3,10 +3,5 @@ package ar.edu.unq.desapp.grupon.backenddesappapi.persistence;
 import org.springframework.data.repository.CrudRepository;
 
 import ar.edu.unq.desapp.grupon.backenddesappapi.Model.Transaction;
-<<<<<<< HEAD
-//public interface ITransactionDao extends CrudRepository<Transaction,Long> {
-//}
-=======
 public interface ITransactionDao extends CrudRepository<Transaction,Long> {
 }
->>>>>>> refactor-and-new-features_E2
