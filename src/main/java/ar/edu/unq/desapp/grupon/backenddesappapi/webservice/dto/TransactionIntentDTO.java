180d7266eb1fe40219c1f18c46c389e988b2a502
Lautaro <lautaro.ml@gmail.com>
2022-06-28 18:36:18 -0300

    arreglando problemas

--- src/main/java/ar/edu/unq/desapp/grupon/backenddesappapi/webservice/dto/TransactionIntentDTO.java
+++ src/main/java/ar/edu/unq/desapp/grupon/backenddesappapi/webservice/dto/TransactionIntentDTO.java
@@ -6,7 +6,7 @@ import ar.edu.unq.desapp.grupon.backenddesappapi.Model.Operation;
 import java.util.Objects;
 
 public class TransactionIntentDTO {
-    CryptoactiveName cryptoactiveName;
+    Cryptoactive cryptoactiveName;
     Float amount;
     Float offer;
     Long userId;
